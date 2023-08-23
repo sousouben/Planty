ab5f0f4651dc37747ab9dc08e2e1f8987c1d4f01
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2023-08-23 11:22:51 +0200

    ajout du logo du site

--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -12,17 +12,11 @@
     <div id="wrapper" class="hfeed">
         <header id="header" role="banner">
             <div id="branding">
-                <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-                    <?php
-                    if (is_front_page() || is_home() || is_front_page() && is_home()) {
-                        echo '<h1>';
-                    }
-                    echo '<a href="' . esc_url(home_url('/')) . '" title="' . esc_attr(get_bloginfo('name')) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html(get_bloginfo('name')) . '</span></a>';
-                    if (is_front_page() || is_home() || is_front_page() && is_home()) {
-                        echo '</h1>';
-                    }
-                    ?>
-                </div>
+                <div id="site-logo">
+                <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name')); ?>" rel="home">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="<?php echo esc_attr(get_bloginfo('name')); ?> Logo">
+            </a>
+                </div>                
                 <div id="site-description" <?php if (!is_single()) {
                                                 echo ' itemprop="description"';
                                             } ?>><?php bloginfo('description'); ?></div>
